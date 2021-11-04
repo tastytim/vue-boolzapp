a906e4568959ea2c7457970998e4a3df8c3e5c7a
tastytim <tastytimgm@gmail.com>
2021-11-04 09:47:54 +0100

    commit 4

--- millestone1/js/app.js
+++ millestone1/js/app.js
@@ -1,10 +1,32 @@
 Vue.config.devtools = true;
 
-const vueApp = new Vue()({
-    el:"#app",
-    
-
+const vueApp = new Vue({
+  el: "#app",
+  data: {
+    users_list: [
+      {
+        name: "Michele",
+        timesent: "04/10/2021 09:00:00",
+        avatar : "img/avatar_1.jpg"
+      },
+      {
+        name: "Fabio",
+        timesent: "04/10/2021 09:00:00",
+        avatar : "img/avatar_2.jpg"
+      },
+      {
+        name: "Samuele",
+        timesent: "04/10/2021 09:00:00",
+        avatar : "img/avatar_3.jpg"
+      },
+      {
+        name: "Luisa",
+        timesent: "04/10/2021 09:00:00",
+        avatar : "img/avatar_4.jpg"
+      },
+      
+    ],
+  },
 });
-
 
 console.dir(vueApp);
